36e21a464d4798968650ffb58364e85768fe1737
Justine Fricou <jfricou@justine.fricou>
2020-02-19 10:35:26 +0100

    ADDED error_detection.c : checks if the pid is a strictly positive int and if the file can be opened and read

--- include/navy.h
+++ include/navy.h
@@ -8,4 +8,9 @@
 #ifndef NAVY_H_
 #define NAVY_H_
 
+// error_detection.c
+int detect_errors(int argc, char **argv);
+int is_incorrect_pid(char *arg);
+int errors_in_file(char *filepath);
+
 #endif /*NAVY_H*/
---
+++ src/error_detection.c
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2020
+** error_detection.c
+** File description:
+** Detects errors in the arguments passed to the program
+*/
+
+#include "navy.h"
+
+int detect_errors(int argc, char **argv)
+{
+    if (argc != 3)
+        return (1);
+    if (is_incorrect_pid(argv[1]))
+        return (1);
+    if (errors_in_file(argv[2]))
+        return (1);
+    return (0);
+}
+
+int is_incorrect_pid(char *arg)
+{
+    char *end = 0;
+    int pid = 0;
+
+    pid = fae_stoi(argv[1], &end);
+    if (*end != 0 || pid <= 0)
+        return (1);
+    return (0);
+}
+
+int errors_in_file(char *filepath)
+{
+    int fd = 0;
+
+    fd = open(filepath, O_RDONLY);
+    if (fd == -1)
+        return (1);
+    // check every line
+    return (0);
+}
